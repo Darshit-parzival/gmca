2d7ba892560d700b0435297eb14f2ec823c4b029
Darshit Rakhasiya <71558362+Darshit-parzival@users.noreply.github.com>
2024-07-25 10:24:37 +0530

    first commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,192 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Government MCA College,Maninagar,Ahmedabad</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+
+    <!-- favicon
+    ============================================ -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/gmca.png">
+
+    <!-- Google Fonts
+    ============================================ -->
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,300,500,600,700,800' rel='stylesheet'
+        type='text/css'>
+
+    <!-- Bootstrap CSS
+    ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">-->
+
+    <!-- Fontawsome CSS
+    ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+
+    <!-- Owl Carousel CSS
+    ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}">
+
+    <!-- jquery-ui CSS
+    ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}">
+
+    <!-- Meanmenu CSS
+    ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/meanmenu.min.css') }}">
+
+    <!-- Animate CSS
+    ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+
+    <!-- Animated Headlines CSS
+    ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/animated-headlines.css') }}">
+
+    <!-- Nivo slider CSS
+            ============================================ -->
+    <!-- <link rel="stylesheet" href="assets/css/nivo-slider.css" type="text/css" />
+    <link rel="stylesheet" href="assets/css/preview.css" type="text/css" media="screen" /> -->
+
+    <!-- Metarial Iconic Font CSS
+    ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/material-design-iconic-font.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/material-design-iconic-font.min.css') }}">
+
+    <!-- Slick CSS
+    ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/slick.css') }}">
+
+    <!-- Style CSS
+    ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <!-- Color CSS
+    ============================================ -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/color.css') }}">
+
+    <!-- Responsive CSS
+    ============================================ -->
+    <link rel="stylesheet" href="../css/responsive.css">
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+
+    <!-- Modernizr JS
+    ============================================ -->
+    <script src="../js/modernizr-2.8.3.min.js"></script>
+    <script src="{{ asset('js/modernizr-2.8.3.min.js') }}"></script>
+
+    <!-- Tab CSS from w3scholl
+    ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/tab_css.css') }}">
+
+    <!-- Gallary css
+    ============================================ -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+
+</head>
+
+<body>
+    <!--Main Wrapper Start-->
+    <div class="as-mainwrapper">
+        <!--Bg White Start-->
+        <div class="bg-white">
+            <!--Header Area Start-->
+            <header>
+                <div class="header-top">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-5 col-md-6 col-sm-7 col-xs-12">
+                                <div class="header-top-right">
+                                    <!-- <div class="content">
+                                            <a href="#"><i class="zmdi zmdi-account"></i> My Account</a>
+                                            <ul class="account-dropdown">
+                                                <li><a href="#">My Account</a></li>
+                                                <li><a href="#">Log In</a></li>
+                                                <li><a href="#">Register</a></li>
+                                                <li><a href="#">Blog</a></li>
+                                            </ul>
+                                        </div> -->
+                                    <!--                                                            <div class="content"><a href="#"><i class="zmdi zmdi-favorite"></i> Wishlist</a></div>
+                                        <div class="content"><a href="#"><i class="zmdi zmdi-shopping-basket"></i> Chechout</a></div>-->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="header-logo-menu sticker headerdisplaynone headerdisplayblock">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-4 col-sm-12">
+                                <div class="logo">
+                                    <!-- <a href="index.php"><img src="img/logo.png" alt="GMCA"></a> -->
+                                    <a href="index.php"><img src=" $Logo " alt="GMCA"></a>
+
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="mainmenu-area pull-right">
+                                    <div class="mainmenu hidden-sm hidden-xs">
+                                        <nav>
+                                            <ul id="nav">
+                                                <li class="current"><a href="index.php">Home</a></li>
+                                                <li><a href="About.php">About</a></li>
+                                                <li><a href="staff.php">Staff</a> </li>
+                                                <li><a href="event.php">Event</a></li>
+                                                <li><a href="Gallery.php">Gallery</a></li>
+                                                <li><a href="academic.php">Academic</a></li>
+                                                <li><a href="contact.php">Contact</a></li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                    <ul class="header-search">
+                                        <li class="search-menu">
+                                            <i id="toggle-search" class="zmdi zmdi-search-for"></i>
+                                        </li>
+                                    </ul>
+                                    <!--Search Form-->
+                                    <div class="search">
+                                        <div class="search-form">
+                                            <form id="search-form" action="#">
+                                                <input type="search" placeholder="Search here..." name="search" />
+                                                <button type="submit">
+                                                    <span><i class="fa fa-search"></i></span>
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    <!--End of Search Form-->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Mobile Menu Area start -->
+                <div class="mobile-menu-area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                <div class="mobile-menu">
+                                    <nav id="dropdown">
+                                        <ul>
+                                            <li><a href="index.php">HOME</a></li>
+                                            <li><a href="About.php">About Us</a></li>
+                                            <li><a href="staff.php">Staff</a></li>
+                                            <li><a href="event.php">Event</a> </li>
+                                            <li><a href="Gallery.php">Gallery</a></li>
+                                            <li><a href="academic.php">Academic</a></li>
+                                            <li><a href="contact.php">Contact</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Mobile Menu Area end -->
+            </header>
