691e07e0e6f62ee886ae1448b40fffacefb7981c
rajsolanki125 <raj6solanki@gmail.com>
2024-09-19 08:44:18 +0530

    visitor count done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Schema;
+use App\Models\VisitCountModel;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +25,30 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Request $request)
     {
-        //
+        if (Schema::hasTable('visit_count_data')) {
+            $userIp = $request->ip();
+
+            // Check if the user IP exists in the database
+            $existingIp = VisitCountModel::where('ip', $userIp)->first();
+
+            if ($existingIp) {
+                $lastAddedTime = strtotime($existingIp->created_at);
+                $twoHoursAgo = strtotime('-2 hours');
+
+                if ($lastAddedTime < $twoHoursAgo) {
+                    VisitCountModel::create(['ip' => $userIp]);
+                }
+            } else {
+                VisitCountModel::create(['ip' => $userIp]);
+            }
+
+            // Get the total count of visitors
+            $totalVisitors = VisitCountModel::count();
+
+            // Share the total visitor count globally
+            View::share('totalVisitors', $totalVisitors);
+        }
     }
 }
