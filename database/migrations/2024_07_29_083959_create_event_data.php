908627e092afbbc0489ecc98424ab0a8187708f1
rajsolanki125 <raj6solanki@gmail.com>
2024-10-09 13:09:08 +0530

    feat: Add 'type' column to event_data and gallery_data tables
    The 'type' column is added to the 'event_data' and 'gallery_data' tables in the database migrations. This column will store the type of event or gallery item.

--- database/migrations/2024_07_29_083959_create_event_data.php
+++ database/migrations/2024_07_29_083959_create_event_data.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('event_data', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50); 
+            $table->string('name', 50);
+            $table->string('type', 50);
             $table->date('date');
             $table->string('report', 30);
             $table->string('details', 500);
